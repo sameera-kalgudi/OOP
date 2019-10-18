260f465ca51fcb0005b1ffa642da06b292c739d1
sameera kalgudi <sk1234@wildcats.unh.edu>
2019-10-18 13:23:59 -0400

    code

---
+++ HW7/src/Spring/CustomerAccountTest.java
@@ -0,0 +1,34 @@
+package Spring;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CustomerAccountTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testCustomerAccount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCreateNewAccount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdateCustomerName() {
+		fail("Not yet implemented");
+	}
+
+}
