dd05899cb4d9cd0c25dd18cf1496de3c6e2e10e8
alextenczar <alextenczar@gmail.com>
2019-10-20 17:58:23 -0400

    updated tests

--- HW7/src/Spring/CustomerAccountTest.java
+++ HW7/src/Spring/CustomerAccountTest.java
@@ -1,7 +1,6 @@
 package Spring;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
 
 import java.sql.SQLException;
 
@@ -16,6 +15,7 @@ class CustomerAccountTest {
 	@BeforeEach
 	void setUp() throws Exception {
 		customerAccount = new CustomerAccount();
+		customerAccount.createNewAccount("1", "2");
 		System.out.println("---Initialized Object---");
 		
 	}
@@ -25,20 +25,28 @@ class CustomerAccountTest {
 	}
 
 	@Test
-	void testCustomerAccount() {
-		fail("Not yet implemented");
+	public void testGetAcctNum() throws SQLException, NoAccountCreatedException {
+		CustomerAccountDAO test = mock(CustomerAccountDAO.class);
+		when(test.getAcctNum(customerAccount)).thenReturn("12");
+		assertEquals(test.getAcctNum(customerAccount), "12");
+		System.out.println("Account Number= " + test.getAcctNum(customerAccount));
+		
 	}
 	
 	@Test
-	void test1() {
+	void testGetAccountName() {
 		CustomerAccountDAO test = mock(CustomerAccountDAO.class);
-		when(test.getAccount("12")).thenReturn();
+		when(test.getAcctName(customerAccount)).thenReturn("1");
+		assertEquals(test.getAcctName(customerAccount), "1");
+		System.out.println("Account Name= " + test.getAcctName(customerAccount));
 	}
 
 	@Test
-	void testCreateNewAccount() {
-		CustomerAccount as = CustomerAccount.createNewAccount("name1", "phone1");
-		CustomerAccount accountTest = CustomerAccountDAO.getAccount("name1phone2");		
+	void testGetAccount() throws SQLException, NoSuchCustomerAccountException {
+		CustomerAccountDAO test = mock(CustomerAccountDAO.class);
+		when(test.getAccount("12")).thenReturn(customerAccount);
+		assertEquals(test.getAccount("12"), customerAccount);
+		System.out.println("Account Name= " + test.getAcctName(customerAccount));	
 	}
 
 	@Test
