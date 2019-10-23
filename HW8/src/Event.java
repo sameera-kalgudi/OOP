7449758039cab29c062fa5c54335abe76d0cf1a4
sameera kalgudi <sk1234@wildcats.unh.edu>
2019-10-22 23:25:35 -0400

    updating files for assignment 8

---
+++ HW8/src/Event.java
@@ -0,0 +1,18 @@
+
+public class Event {
+	private int EventNumber;
+	private int EventData;
+	
+	public int getEventData() {
+		return 1;
+	}
+	
+	public int getEventNumber() {
+		return 1;
+	}
+	
+	public void Event(int EventNumber, int EventData){
+		
+	}
+
+}
