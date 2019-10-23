70aff398dcbed08980c7ddb78aec69b621def50e
jacob-blomq <35968186+jacob-blomq@users.noreply.github.com>
2019-10-23 16:45:36 -0400

    Update PublisherImplementation.java

--- HW8/src/PublisherImplementation.java
+++ HW8/src/PublisherImplementation.java
@@ -1,15 +1,41 @@
-
-public class PublisherImplementation {
+import java.util.*;
+public class PublisherImplementation implements PublisherInterface {
 	
+
+	private List<Observer> subscribers = new ArrayList<Observer>();
+	private List<Observer> unsubscribers = new ArrayList<Observer>();
+ 	
+
 	public void notifyObserver(Event e) {
 		
 	}
 	
-	public Event generateEvent() {
-		
+	private Event generateEvent() {
+		Event e = new Event(1, 1);
 	}
 	
 	public void runSimulation() {
 		
 	}
+
+	public void registerObserver(Observer o) {
+		
+		if (unsubscribers.contains(o) {
+			unsubscribers.remove(o);
+		}
+		
+		subscribers.add(o);
+	}
+
+	public void removeObserver(Observer o) {
+	
+		if (subscribers.contains(o) {
+			subscribers.remove(o);
+		}
+		
+		subscribers.remove(o);
+
+	}
+
+
 }
